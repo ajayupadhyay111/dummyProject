df1e74a8eb6b26090afa8876bb670cbfb1de80a8
ajayupadhyay111 <upadhyayajay156@gmail.com>
2025-03-04 15:33:02 +0530

    versel.json file added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
-import express, { application } from 'express'
-import {config} from 'dotenv'
-import connectDB from './utils/db.js';
+import express, { application } from "express";
+import { config } from "dotenv";
+import connectDB from "./utils/db.js";
 config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.end("hello world");
+});
 
-app.get('/',(req,res)=>{res.end("hello world")});
-
-app.listen(process.env.PORT,()=>{
-    connectDB();
-    console.log("server is running on port ",process.env.PORT);
-})
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log("server is running on port ", process.env.PORT);
+});
